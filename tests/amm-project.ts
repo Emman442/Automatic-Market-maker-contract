1ca4d1d64b534e853ccd3002e6216d12fd1436d9
fido1hn <confidoekeanya@gmail.com>
2025-03-26 22:56:53 +0100

    first instruction test passed

--- tests/amm-project.ts
+++ tests/amm-project.ts
@@ -21,74 +21,75 @@ describe("amm-project", () => {
       program.programId
     );
 
-  const mintA = Keypair.generate();
-  console.log("Mint A Keypair created:", mintA.publicKey.toString());
-  const mintB = Keypair.generate();
-  console.log("Mint B Keypair created:", mintB.publicKey.toString());
+  let mintA = Keypair.generate();
+  // console.log("Mint A Keypair created:", mintA.publicKey.toString());
+  let mintB = Keypair.generate();
+  // console.log("Mint B Keypair created:", mintB.publicKey.toString());
   const lpMint = Keypair.generate();
   console.log("LP Mint Keypair created:", lpMint.publicKey.toString());
   let poolAccount: PublicKey;
   let poolBump: number;
 
   before(async () => {
-    await createMint(
-      provider.connection,
-      wallet.payer, // Payer of the transaction
-      wallet.publicKey, // Mint authority
-      null, // Freeze authority (optional)
-      6, // Decimals
-      mintA // Mint Keypair
-    );
-    console.log("Mint A created:", mintA.publicKey.toString());
+    // mintA = await createMint(
+    //   provider.connection,
+    //   wallet.payer, // Payer of the transaction
+    //   wallet.publicKey, // Mint authority
+    //   null, // Freeze authority (optional)
+    //   6 // Decimals
+    // );
+    console.log("Mint A created:", mintA.toString());
 
     // Create mintB
-    await createMint(
-      provider.connection,
-      wallet.payer,
-      wallet.publicKey,
-      null,
-      6,
-      mintB
+    // mintB = await createMint(
+    //   provider.connection,
+    //   wallet.payer,
+    //   wallet.publicKey,
+    //   null,
+    //   6
+    // );
+    console.log("Mint B created:", mintB.toString());
+
+    [poolAccount, poolBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("pool"),
+        mintA.publicKey.toBuffer(),
+        mintB.publicKey.toBuffer(),
+      ],
+      program.programId
     );
-    console.log("Mint B created:", mintB.publicKey.toString());
+
+    const vaultA = getAssociatedTokenAddressSync(
+      mintA.publicKey,
+      poolAccount, // Use poolAccount as the owner
+      true // Allow owner to be a PDA
+    );
+
+    const vaultB = getAssociatedTokenAddressSync(
+      mintB.publicKey,
+      poolAccount, // Use poolAccount as the owner
+      true // Allow owner to be a PDA
+    );
   });
 
-  [poolAccount, poolBump] = anchor.web3.PublicKey.findProgramAddressSync(
-    [
-      Buffer.from("pool"),
-      mintA.publicKey.toBuffer(),
-      mintB.publicKey.toBuffer(),
-    ],
-    program.programId
-  );
-  const vaultA = getAssociatedTokenAddressSync(
-    mintA.publicKey,
-    poolAccount, // Use poolAccount as the owner
-    true // Allow owner to be a PDA
-  );
-
-  const vaultB = getAssociatedTokenAddressSync(
-    mintB.publicKey,
-    poolAccount, // Use poolAccount as the owner
-    true // Allow owner to be a PDA
-  );
-
   it("Initialize config", async () => {
-    const tx = await program.methods
-      .processInitializeConfig(new anchor.BN(42), new anchor.BN(4))
-      .accountsStrict({
-        tokenProgram: TOKEN_PROGRAM_ID,
-        mintA: mintA.publicKey,
-        signer: wallet.publicKey,
-        mintB: mintB.publicKey,
-        configAccount: configAccount,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .signers([wallet.payer, mintA, mintB])
-      .rpc()
-      .catch((e) => console.log("Error", e));
-
-    console.log("Initialize config", tx);
+    try {
+      const tx = await program.methods
+        .processInitializeConfig(new anchor.BN(42), new anchor.BN(4))
+        .accountsStrict({
+          mintA: mintA.publicKey,
+          mintB: mintB.publicKey,
+          configAccount: configAccount,
+          signer: wallet.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([wallet.payer, mintA, mintB])
+        .rpc();
+      console.log("Transaction successful, here is your ===>", tx);
+    } catch (error) {
+      console.log("There was an error", error);
+    }
   });
 
   it("Initialize pool", async () => {
