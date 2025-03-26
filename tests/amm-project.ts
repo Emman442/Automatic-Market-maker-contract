e408a20c0f56ec1e76a04d8a45f0a5e43f166ce9
fido1hn <confidoekeanya@gmail.com>
2025-03-26 22:26:30 +0100

    added console log to show problem

--- tests/amm-project.ts
+++ tests/amm-project.ts
@@ -3,7 +3,11 @@ import { Program } from "@coral-xyz/anchor";
 import { AmmProject } from "../target/types/amm_project";
 import { TOKEN_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
 import { Keypair, PublicKey } from "@solana/web3.js";
-import { createAssociatedTokenAccount, createMint, getAssociatedTokenAddressSync } from "@solana/spl-token"
+import {
+  createAssociatedTokenAccount,
+  createMint,
+  getAssociatedTokenAddressSync,
+} from "@solana/spl-token";
 
 describe("amm-project", () => {
   const provider = anchor.AnchorProvider.env();
@@ -11,23 +15,22 @@ describe("amm-project", () => {
   const wallet = provider.wallet as anchor.Wallet;
   const program = anchor.workspace.ammProject as Program<AmmProject>;
 
-  const [configAccount, configBump] = anchor.web3.PublicKey.findProgramAddressSync(
-    [
-      Buffer.from("config"),
-      wallet.publicKey.toBuffer()
-    ],
-    program.programId
-  );
+  const [configAccount, configBump] =
+    anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("config"), wallet.publicKey.toBuffer()],
+      program.programId
+    );
 
   const mintA = Keypair.generate();
+  console.log("Mint A Keypair created:", mintA.publicKey.toString());
   const mintB = Keypair.generate();
-  const lpMint =Keypair.generate();
+  console.log("Mint B Keypair created:", mintB.publicKey.toString());
+  const lpMint = Keypair.generate();
+  console.log("LP Mint Keypair created:", lpMint.publicKey.toString());
   let poolAccount: PublicKey;
   let poolBump: number;
 
   before(async () => {
-  
-
     await createMint(
       provider.connection,
       wallet.payer, // Payer of the transaction
@@ -36,6 +39,7 @@ describe("amm-project", () => {
       6, // Decimals
       mintA // Mint Keypair
     );
+    console.log("Mint A created:", mintA.publicKey.toString());
 
     // Create mintB
     await createMint(
@@ -46,53 +50,51 @@ describe("amm-project", () => {
       6,
       mintB
     );
-
-  
+    console.log("Mint B created:", mintB.publicKey.toString());
   });
 
   [poolAccount, poolBump] = anchor.web3.PublicKey.findProgramAddressSync(
     [
       Buffer.from("pool"),
       mintA.publicKey.toBuffer(),
-      mintB.publicKey.toBuffer()
+      mintB.publicKey.toBuffer(),
     ],
     program.programId
   );
   const vaultA = getAssociatedTokenAddressSync(
     mintA.publicKey,
-    poolAccount,  // Use poolAccount as the owner
-    true  // Allow owner to be a PDA
+    poolAccount, // Use poolAccount as the owner
+    true // Allow owner to be a PDA
   );
 
   const vaultB = getAssociatedTokenAddressSync(
     mintB.publicKey,
-    poolAccount,  // Use poolAccount as the owner
-    true  // Allow owner to be a PDA
+    poolAccount, // Use poolAccount as the owner
+    true // Allow owner to be a PDA
   );
 
   it("Initialize config", async () => {
     const tx = await program.methods
       .processInitializeConfig(new anchor.BN(42), new anchor.BN(4))
-      .accounts({
+      .accountsStrict({
         tokenProgram: TOKEN_PROGRAM_ID,
         mintA: mintA.publicKey,
         signer: wallet.publicKey,
         mintB: mintB.publicKey,
-        //@ts-ignore
         configAccount: configAccount,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
       .signers([wallet.payer, mintA, mintB])
-      .rpc().catch(e=>console.log("Error", e));
+      .rpc()
+      .catch((e) => console.log("Error", e));
 
     console.log("Initialize config", tx);
   });
 
   it("Initialize pool", async () => {
-
     const tx = await program.methods
       .initializePool()
-      .accounts({
+      .accountsStrict({
         tokenProgram: TOKEN_PROGRAM_ID,
         mintA: mintA.publicKey,
         signer: wallet.publicKey,
@@ -106,7 +108,8 @@ describe("amm-project", () => {
         systemProgram: anchor.web3.SystemProgram.programId,
       })
       .signers([wallet.payer, lpMint])
-      .rpc({ skipPreflight: true }).catch(e => console.log("Error2", e));;
+      .rpc({ skipPreflight: true })
+      .catch((e) => console.log("Error2", e));
 
     console.log("Initialize pool", tx);
   });
